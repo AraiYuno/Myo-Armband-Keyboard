482f6813800c69c76d2d6cbcb5f36769442f45cc
AraiYuno <pyh2982@gmail.com>
2018-03-08 21:03:33 -0600

    Updated get_input_x function

--- Q3/Myo Keyboard/Interval.py
+++ Q3/Myo Keyboard/Interval.py
@@ -1,3 +1,12 @@
+#=====================================================================================================#
+#                                                                                                     #
+# Interval.py                                                                                         #
+#                                                                                                     #
+# Author: Kyle                                                                                        #
+# Purpose: This .py file contains a class called "Interval" which stores a start_time and end_time of #
+#          an interval that can be used to separate sets of input                                     #
+#=====================================================================================================#
+
 class Interval:
     def __init__(self, start_time, end_time):
         self.start_time = start_time
